43b8b97c7a4807944b88996cb9e590dfb4799c80
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-18 21:40:54 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-  resources :restaurants, only: %i[index show] do
-    resources :bookings, only: %i[create]
-  end
-  # Defines the root path route ("/")
-  namespace :host do
-    resources :stars, only: [:new, :create]
-
-    resources :bookings, only: [] do
-      member do
-        patch :accept
-        patch :decline
-      end
-    end
-
-  end
-
-  resource :dashboard, only: :show
+  
 end
   # root "articles#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_200322) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string "nom"
-    t.string "adresse"
-    t.integer "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
